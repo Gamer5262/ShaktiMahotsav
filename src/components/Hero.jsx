ddb8c8937f3fb2d8db67c90dbe3b3b0c9d817299
Tarun R G <tarungovindharaj2003@gmail.com>
2023-10-08 10:18:23 +0530

    updated ui

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,113 +1,56 @@
-import styled from "styled-components";
-
-const Section = styled.div`
-  height: 100vh;
-  scroll-snap-align: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-evenly;
-  @media only screen and (max-width: 768px) {
-    height: 200vh;
-  }
-`;
-
-const Container = styled.div`
-  height: 80%;
-  scroll-snap-align: center;
-  width: 98vw;
-  display: flex;
-  justify-content: space-around;
-  @media only screen and (max-width: 768px) {
-    width: 100%;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-`;
-
-const Left = styled.div`
-  flex: 2;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  max-width: 45vw;
-  gap: 20px;
-  @media only screen and (max-width: 768px) {
-    flex: 1;
-    align-items: center;
-    max-height: 80vw;
-  }
-`;
-
-const Title = styled.h1`
-  font-size: 60px;
-  color: #ff6611;
-  font-family: 'Kaushan Script', cursive;
-  max-width: 600px;
-  @media only screen and (max-width: 768px) {
-    font-size: 45px;
-    text-align: center;
-  }
-`;
-
-const Desc = styled.p`
-  font-family: 'Inclusive Sans', sans-serif;
-  text-align: justify;
-  font-size: 20px;
-  max-width: 500px;
-  color: lightgray;
-  @media only screen and (max-width: 768px) {
-    padding: 20px 20px 40px 20px;
-    min-width: 300px;
-    width: 100%; 
-    text-align: center;
-  }
-`;
-
-
-const Right = styled.div`
-  width: 40vw;
-  @media only screen and (max-width: 768px) {
-    margin-top: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-`;
-
-
-const Img = styled.img`
-  width: 400px;
-  height: auto;
-  object-fit: contain;
-  animation: animate 2s infinite ease alternate;
-  @media only screen and (max-width: 768px) {
-    width: 300px;
-    height: 400px;
-  }
-  @keyframes animate {
-    to {
-      transform: translateY(20px);
-    }
-  }
-`;
+import './Hero.css';
+import Carousel from 'react-bootstrap/Carousel';
+import Img1 from '../assets/img5.avif';
+import Img2 from '../assets/img6.png';
+import Img3 from '../assets/img7.jpg';
+import Navbar from './Navbar';
 
 const Hero = () => {
   return (
-    <Section>
-      <Container>
-        <Left>
-          <Title>Shaktimahotsav</Title>
-          <Desc>
-            Join our Navratri celebrations and experience the spirit of devotion, music, and dance. Let&#39;s celebrate together!.
-          </Desc>
-        </Left>
-        <Right>
-            <Img src="./assets/hero-img.png" />
-        </Right>
-      </Container>
-    </Section>
+    <div>
+      <Navbar />
+      <div className='slides'>
+        <Carousel >
+          
+          <Carousel.Item>
+            <img
+              className="d-block w-100 img-slider"
+              src={Img1}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h5>First slide label</h5>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100 img-slider"
+              src={Img2}
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h5>Second slide label</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <button>Click</button>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100 img-slider"
+              src={Img3}
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h5>Third slide label</h5>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </div>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,119 +1,39 @@
-import styled from "styled-components";
-import { useState } from 'react';
-import { Link } from "react-router-dom";
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.css";
 
-const Section = styled.div`
-  display: flex;
-  justify-content: center;
-  @media only screen and (max-width: 768px) {
-    width: 100%;
-  }
-`;
+function Navbar() {
+	const navRef = useRef();
 
-const Container = styled.div`
-  width: 1400px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  @media only screen and (max-width: 768px) {
-    width: 100%;
-    padding: 10px;
-  }
-`;
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
 
-const Links = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 50px;
-`;
-
-const List = styled.ul`
-  display: flex;
-  gap: 50px;
-  list-style: none;
-
-  @media only screen and (max-width: 768px) {
-    display: none;
-  }
-`;
-
-
-const H2 = styled.h2`
-  font-size: 25px;
-  color: #ff6611; 
-  margin-left: 20px;
-  font-family: 'Kaushan Script', cursive;
-  @media only screen and (max-width: 768px) {
-    text-align: center;
-  }
-`;
-
-const ListItem = styled.li`
-  cursor: pointer;
-  font-family: 'Cabin', sans-serif;
-  display: inline-block;
-  position: relative;
-  color: #f1f1f1;
-  &:hover {
-    color: #ff5533;
-    transform: scaleX(1);
-    transform-origin: bottom left;
-  }
-  &:hover::after {
-    content: '';
-    position: absolute;
-    width: 100%;
-    transform: scaleX(0);
-    height: 2px;
-    bottom: 0;
-    left: 0;
-    background-color: #0087ca;
-    transform-origin: bottom right;
-    transition: transform 0.25s ease-out;
-  }
-
-`;
-
-const Icons = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 20px;
-`;
-
-const Button = styled.button`
-  font-family: 'Cabin', sans-serif;
-  padding: 10px 15px;
-  background-color: #ff6611;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-`; 
-
-const Navbar = ({ change }) => {
-  const [value, setNewValue] = useState("");
-   function handleChange(event) {
-      let value = event.target.value;
-      setNewValue(value);
-      change(value);
-   }
-  return (
-    <Section>
-      <Container>
-        <Links>
-          <H2 >ShakthiMahotsav</H2>
-          <List>
-            <Link to="/"><ListItem>Home</ListItem></Link>
-            <Link to="/about"><ListItem>About</ListItem></Link>
-            <Link to="/team"><ListItem>Team</ListItem></Link>
-          </List>
-        </Links>
-        <Icons>
-          <Button onClick={handleChange}>Register</Button>
-        </Icons> 
-      </Container>
-    </Section>
-  );
-};
+	return (
+		<header className="navbar">
+			<h3>Shakthimahotsav</h3>
+			<nav ref={navRef}>
+				<a href="#">Home</a>
+        		<a href="#about">About</a>
+        		<a href="#events">Events</a>
+				<a href="#gallery">Gallery</a>
+        		<a href="#team">Team</a>
+        		<a href="#contact">Contact</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
+}
 
 export default Navbar;
