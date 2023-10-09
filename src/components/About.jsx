0e7b126b27345b4a698a7add9b7177fc32a7178c
Gamer5262 <priyanshuaggarwal2003@gmail.com>
2023-10-09 11:48:52 +0530

    About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,20 @@
-
+import "./About.css";
+import img1 from "../assets/img3.avif";
 
 function About() {
   return (
-    <div id="about">
-      About
+    <div className='about' id='about'>
+      <div className='section-about'>
+        <h4>Navratri</h4>
+        <div className="content">
+        <p>
+          Celebrating the victory of good over evil.
+
+          Something something...
+           </p>
+        <img src={img1} className="logo"></img>
+        </div>
+      </div>
     </div>
   )
 }
